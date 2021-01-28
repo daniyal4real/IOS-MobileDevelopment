e13dd30c4d9ecc23da394e80d2b24c126599df1c
Daniyal <56621752+daniyal4real@users.noreply.github.com>
2021-01-28 15:34:04 +0600

    Add files via upload

---
+++ iamrich/iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Daniyal on 1/13/21.
+//  Copyright © 2021 Daniyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
